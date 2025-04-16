0a313519cf29eee265d62592e81fa56eaa1bf6a7
StellRC <stellaffarce@gmail.com>
2025-04-16 10:08:42 -0700

    Updated form and table CSS and started on readme

--- readme.md
+++ readme.md
@@ -1,9 +1,6 @@
 The cursor was inspired by [Google Design](https://design.google/library/lexend-readability)
 
 
-<!-- Make a popup with all the row info laid out nicely for viewing  -->
-<!-- Presents, toggle for favorite, birthday, name -->
-<!-- Sort by month and day -->
-<!-- Toggle for calendar view -->
-<!-- Choose presents from drop down with each drop down item attached to svg image -->
-<!-- Age -->
+The Reset was inspired by 
+
+This site uses Lexend which is good for readability 
--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,11 +1,11 @@
-const cursorBig = document.querySelector(".cursor-big");
+const customCursor = document.querySelector(".custom-cursor");
 
 const positionElement = (e) => {
   // Page offset enables custom cursor to follow mouse beyond 100vh
   const mouseY = e.clientY + window.pageYOffset;
   const mouseX = e.clientX + window.pageXOffset;
 
-  cursorBig.style.transform = `translate3d(${mouseX + -15}px, ${
+  customCursor.style.transform = `translate3d(${mouseX + -15}px, ${
     mouseY + -15
   }px, 0)`;
 };
